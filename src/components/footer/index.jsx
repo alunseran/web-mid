5f90231e1ad50f25b06802c92762d642dcce5010
alunseran <alunseran@unklab.ac.idgit config --global user.email s22210435@student.unklab.ac.id>
2024-11-21 10:14:00 +0700

    midweb

---
+++ src/components/footer/index.jsx
@@ -0,0 +1,21 @@
+const Footer = () => {
+    return (
+      <footer className="page-footer">
+        <div className="container">
+          <div className="social-icons">
+            <a href="https://web.facebook.com/profile.php?id=61555829860523">
+              <i className="ion-social-facebook" title="Facebook" />
+            </a>
+            <a href="https://www.instagram.com/alunthoo/">
+              <i className="ion-social-instagram-outline" title="Instagram" />
+            </a>
+            <a href="#">
+              <i className="ion-social-twitter" title="Twitter" />
+            </a>
+          </div>
+        </div>
+      </footer>
+    );
+}
+
+export default Footer;
